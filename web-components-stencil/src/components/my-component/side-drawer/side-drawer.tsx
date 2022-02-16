e420eab4f2b21977217451c0b135b5c26fbcdd24
jsprofessional <jshuman@protonmail.com>
2022-02-16 16:59:47 -0600

    Coded along through s7e83

--- web-components-stencil/src/components/my-component/side-drawer/side-drawer.tsx
+++ web-components-stencil/src/components/my-component/side-drawer/side-drawer.tsx
@@ -1,1 +1,14 @@
-class SideDrawer
+import { Component } from '@stencil/core';
+
+@Component({
+    tag: 'uc-side-drawer'
+})
+export class SideDrawer {
+	render() {
+		return {
+			<div>
+				<h1>The Side Drawer</h1>
+			</div>
+		};
+	}
+}
